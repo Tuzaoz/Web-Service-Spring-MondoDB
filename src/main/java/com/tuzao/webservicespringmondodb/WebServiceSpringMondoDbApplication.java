79ae4c1f65058bff3d967ce1bed1ea2a05e37e22
Arthur Dantas <arthurdantaspro@gmail.com>
2022-12-05 11:18:02 -0300

    Projeto Criado

---
+++ src/main/java/com/tuzao/webservicespringmondodb/WebServiceSpringMondoDbApplication.java
@@ -0,0 +1,13 @@
+package com.tuzao.webservicespringmondodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceSpringMondoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceSpringMondoDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuzao/webservicespringmondodb/WebServiceSpringMondoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuzao.webservicespringmondodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceSpringMondoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
